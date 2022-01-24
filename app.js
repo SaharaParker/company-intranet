c844c9145455768ff54bd7825d08fddb9b9872ab
Sahara <THsahara@gmail.com>
2022-01-24 10:01:30 -0500

    got preventDefault working

---
+++ app.js
@@ -0,0 +1,9 @@
+const div = document.getElementById('errorMsg')
+const form = document.getElementById('login')
+const passwordInput = document.getElementById('phonenumber');
+const emailInput = document.getElementById('email');
+
+form.addEventListener('submit',(evt)=> {
+    evt.preventDefault();
+    console.log("hello");
+})
