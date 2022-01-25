949e6686f22b0b61bffb6236b19609ac471e0ac6
Sahara <THsahara@gmail.com>
2022-01-25 14:01:55 -0500

    added alert and logo,border to index.html

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const div = document.getElementById('errorMsg')
+const errorDiv = document.getElementById('errorMsg')
 const form = document.getElementById('login')
-const passwordInput = document.getElementById('phonenumber');
+const passwordInput = document.getElementById('password');
 const emailInput = document.getElementById('email');
 
 form.addEventListener('submit',(evt)=> {
     evt.preventDefault();
-    console.log("hello");
+    checkInputIsEmpty(passwordInput);
+    checkInputIsEmpty(emailInput);
+    // console.log("hello");
 })
+
+function checkInputIsEmpty (input){
+    // console.log("hello")
+    if (input.value === ""){
+        // alert("Email or Password Required!");
+        const errorMessage = document.createElement('div');
+        errorMessage.innerHTML = '<p class="p-6 bg-red-500 text-white font-bold">Email or Password required.</p>';
+        errorDiv.appendChild(errorMessage);
+    }
+}
