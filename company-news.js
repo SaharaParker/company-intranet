4de0373f0790b33b07096d25220424dd749dc33f
Sahara <THsahara@gmail.com>
2022-01-26 18:02:16 -0500

    added 2 news divs

--- company-news.js
+++ company-news.js
@@ -2,7 +2,7 @@
 const news1 = {
     fullName: "Alice Atom",
     date: "12-27-2021",
-    content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+    content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequuntur, earum eius eveniet id iusto libero, nihil nostrum omnis perspiciatis, qui quidem sed voluptas. Asperiores deserunt ex itaque iusto labore libero maxime nam nemo, pariatur quia reiciendis repellendus saepe sed ullam velit. Aspernatur ducimus eius enim officia repellat rerum temporibus!"
 };
 
 const news2 = {
