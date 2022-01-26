4371dac494533be573da0dfb18dacabf26b9ca4f
Sahara <THsahara@gmail.com>
2022-01-26 18:27:26 -0500

    am able to all news as an array in my console

--- company-news.js
+++ company-news.js
@@ -1,24 +1,39 @@
+//
+// const news1 = {
+//     fullName: "Alice Atom",
+//     date: "12-27-2021",
+//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+// };
+//
+// const news2 = {
+//     fullName:"Blake Bleak",
+//     date:"12-28-2021",
+//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+// };
+//
+// const news3 = {
+//     fullName:"Cara Cole",
+//     date:"12-29-2021",
+//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+// };
+//
+// console.log(news1);
+// console.log(news2);
+// console.log(news3);
 
-const news1 = {
-    fullName: "Alice Atom",
-    date: "12-27-2021",
-    content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequuntur, earum eius eveniet id iusto libero, nihil nostrum omnis perspiciatis, qui quidem sed voluptas. Asperiores deserunt ex itaque iusto labore libero maxime nam nemo, pariatur quia reiciendis repellendus saepe sed ullam velit. Aspernatur ducimus eius enim officia repellat rerum temporibus!"
-};
 
-const news2 = {
-    fullName:"Blake Bleak",
-    date:"12-28-2021",
-    content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
-};
+const allNews = [{
+        fullName: "Alice Atom",
+        date: "12-27-2021",
+        content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+        }, {fullName:"Blake Bleak",
+            date:"12-28-2021",
+            content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+        }, {fullName:"Cara Cole",
+            date:"12-29-2021",
+            content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+}];
 
-const news3 = {
-    fullName:"Cara Cole",
-    date:"12-29-2021",
-    content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
-};
-
-console.log(news1);
-console.log(news2);
-console.log(news3);
+console.log(allNews);
 
 
