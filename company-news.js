d9631ead92d75108438fb8391f39a22a741be1a8
Sahara <THsahara@gmail.com>
2022-01-27 08:41:47 -0500

    added form add-news to company-news.js

--- company-news.js
+++ company-news.js
@@ -1,26 +1,3 @@
-//
-// const news1 = {
-//     fullName: "Alice Atom",
-//     date: "12-27-2021",
-//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
-// };
-//
-// const news2 = {
-//     fullName:"Blake Bleak",
-//     date:"12-28-2021",
-//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
-// };
-//
-// const news3 = {
-//     fullName:"Cara Cole",
-//     date:"12-29-2021",
-//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
-// };
-//
-// console.log(news1);
-// console.log(news2);
-// console.log(news3);
-
 const allNews = [{
         fullName: "Alice Atom",
         date: "12-27-2021",
@@ -30,12 +7,19 @@ const allNews = [{
             content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
         }, {fullName:"Cara Cole",
             date:"12-29-2021",
-            content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
+            content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!",
+        },
+            {   fullName: "Drew Dean",
+                date: "12-30-2021",
+                content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut consequatur ducimus eveniet facere id, ipsa iusto provident quas veritatis!"
 }]; //made allnews into an array(list)
 
 console.log(allNews);
 
 const newsShow = document.getElementById('compnews');
+
+const form = document.getElementById('add-news');
+
 for(const key in allNews){
     const newsExist = document.createElement('div');
 
@@ -49,4 +33,5 @@ for(const key in allNews){
 
     newsShow.appendChild(newsExist); //adds new div to existing div(in html)
 }
+
 
